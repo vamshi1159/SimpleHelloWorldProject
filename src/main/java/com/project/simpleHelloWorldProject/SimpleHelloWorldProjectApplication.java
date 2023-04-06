e288635143d639cb649cddd660765e08fe802cbe
vamshi krishna <vamshikrishna.egideli@zemosolabs.com>
2023-04-06 19:19:30 +0530

    initial

---
+++ src/main/java/com/project/simpleHelloWorldProject/SimpleHelloWorldProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.simpleHelloWorldProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleHelloWorldProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleHelloWorldProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/simpleHelloWorldProject/SimpleHelloWorldProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.simpleHelloWorldProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleHelloWorldProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
